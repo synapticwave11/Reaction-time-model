1f915b309a490fca15eb737af811e8c2cf9df171
Alia <singhalia2008@gmail.com>
2026-03-08 12:28:32 +0530

    Update RTmodel_plot.py

--- RTmodel_plot.py
+++ RTmodel_plot.py
@@ -6,6 +6,8 @@ df= pd.read_csv("reaction_time.csv")
 
 # calculate average reaction time for each condition
 avg_rt =df.groupby("choices")["reaction_time"].mean()
+print(df.head(20))
+print(df["choices"].value_counts())
 
 print(avg_rt)
 
@@ -15,5 +17,6 @@ plt.plot(avg_rt.index, avg_rt.values, marker='o')
 plt.title("Hick's Law Simulation")
 plt.xlabel("Number of choices")
 plt.ylabel("Reaction Time (ms)")
+
 
 plt.show()
