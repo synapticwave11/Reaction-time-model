fc3eb26043db1a1ddd2ae90ba369ea9b6d5f6352
Alia <singhalia2008@gmail.com>
2026-03-09 22:56:39 +0530

    Update RTmodel_plot.py

--- RTmodel_plot.py
+++ RTmodel_plot.py
@@ -1,21 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
 # load dataset
 df = pd.read_csv("reaction_time.csv")
-
 # sort values so the graph connects properly
 df = df.sort_values("choices")
-
 # plot
 plt.figure()
 plt.plot(df["choices"], df["reaction_time"], marker='o')
-
 plt.title("Reaction Time vs Number of Choices")
 plt.xlabel("Number of Choices")
 plt.ylabel("Reaction Time (ms)")
 plt.grid(True)
 plt.show()
+
 
 
 
