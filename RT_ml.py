75beb09d8153cec99d375edb0ce801892e1d1ae4
Alia <singhalia2008@gmail.com>
2026-03-14 17:15:12 +0530

    Update RT_ml.py

--- RT_ml.py
+++ RT_ml.py
@@ -24,4 +24,4 @@ log_value=np.log2(new_choices)
 new_data = pd.DataFrame({"log_choices": [log_value]})
 
 prediction = model.predict([[new_data]])
-print(f"Predicted reaction time for {new_choices} choic
+print(f"Predicted reaction time
