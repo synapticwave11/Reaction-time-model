a0727f10297d0cd220920dec79c6bb81e012af15
Alia <singhalia2008@gmail.com>
2026-03-22 18:54:37 +0530

    Update rt_simulation.py

--- rt_simulation.py
+++ rt_simulation.py
@@ -13,7 +13,6 @@ for choices in choices_list:
                  reaction_time = 200 + 150 *  np.log2(choices)
 
                  #add random noise
-      
                  reaction_time += np.random.normal(0,20)
                  data.append([choices,reaction_time])
 
